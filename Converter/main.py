6c5ca1ec90715843b4ba0ae674c2df2027e65420
ayanalikhan4 <74181385+ayanalikhan4@users.noreply.github.com>
2022-09-17 10:20:10 -0400

    Update main.py

--- Converter/main.py
+++ Converter/main.py
@@ -1,13 +1,38 @@
 from curses import window
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.title("My first GUI Progame")
-window.minsize(width = 500, height=300)
+window = Tk()
+window.title("Mile to KM Converter")
+window.minsize(width = 200, height=100)
+window.config(padx =20 , pady = 20)
 
-my_lable = tkinter.Label(text = "I am a lable", font=("Ariel", 24, "bold"))
-my_lable.pack()
-# this a temp
+
+miles_input = Entry(width = 10,)
+miles_input.grid(column=2, row = 0)
+
+
+# Lable
+miles_lable = Label(text = "Miles", font=("Ariel", 24, "bold"))
+miles_lable.grid(column=3, row = 0)
+
+# Lable
+is_equal_lable = Label(text = "is equal to", font=("Ariel", 24, "bold"))
+is_equal_lable.grid(column=0, row = 1)
+
+km_result_lable = Label(text = "0", font=("Ariel", 24, "bold"))
+km_result_lable.grid(column=2, row = 1)
+
+km_text_lable = Label(text = "KM", font=("Ariel", 24, "bold"))
+km_text_lable.grid(column=3, row = 1)
+
+def button_on_click():
+    miles_input_value =  miles_input.get()
+    converted_km_value = int(miles_input_value)*1.609
+    km_result_lable.config(text = converted_km_value)
+
+
+calculate_button = Button(text =  "Calculate", command=button_on_click)
+calculate_button.grid(column=2, row = 3)
 
 
 window.mainloop()
