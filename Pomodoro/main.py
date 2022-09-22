a9b4a0e5a955f10bbcbb68b62d41304253fd0429
ayanalikhan4 <74181385+ayanalikhan4@users.noreply.github.com>
2022-09-21 20:02:59 -0400

    Main Updated

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import * 
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,15 +9,28 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    global reps
+    work_secs = WORK_MIN*60
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 import time
 
+def count_down(count):
+
+    count_min = math.floor(count/60)
+    count_sec = count%60
+    if count_sec < 10:
+        count_sec= f"0{count_sec}"
+
+    canvas.itemconfig(timer_text,text =f"{count_min}:{count_sec}")
+    if count >0:
+        window.after(1000,count_down, count-1)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -29,6 +43,7 @@ def say_something():
     print("Something")
 
 
+
 window.after(5000, say_something)
 
 title_lable = Label(text = "Timer", fg = GREEN,bg= YELLOW, font= (FONT_NAME,50))
@@ -38,8 +53,11 @@ canvas = Canvas(width=200, height = 224, bg= YELLOW, highlightthickness= 0)
 tomato_img = PhotoImage(file="/Users/ayanalikhan/Desktop/CS/Development/Projects/Pomodoro/tomato.png")
 canvas.create_image(100,112, image = tomato_img )
 
-canvas.create_text(100,130, text = "00:00", fill = "white",font = (FONT_NAME,35,"bold"))
+
+timer_text = canvas.create_text(100,130, text = "00:00", fill = "white",font = (FONT_NAME,35,"bold"))
 canvas.grid(row = 1, column= 1)
+
+
 
 def button_on_click():
     pass
@@ -47,7 +65,7 @@ def button_on_click():
 reset_button = Button(text = "Reset",command=button_on_click, highlightthickness= 0)
 reset_button.grid(row = 2, column= 0)
 
-start_button = Button(text = "Start",command=button_on_click,highlightthickness= 0)
+start_button = Button(text = "Start",command=start_timer,highlightthickness= 0)
 start_button.grid(row = 2, column= 2)
 
 check_marks = Label(text = "✔",fg= GREEN,bg=YELLOW)
