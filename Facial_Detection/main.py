b47110a5fe12080f681bbc588b9af20f27d3a82e
ayanalikhan4 <74181385+ayanalikhan4@users.noreply.github.com>
2022-10-15 01:04:56 -0400

    Update main.py

--- Facial_Detection/main.py
+++ Facial_Detection/main.py
@@ -1,7 +1,7 @@
 from deepface import DeepFace
 from IPython.display import Image 
 
-path = "Mark-Zuckerberg-012.jpg"
+path = "/Users/ayanalikhan/Desktop/CS/Development/Projects/Facial_Detection/IMG_1654.jpg"
 Image(filename=path)
 
 obj = DeepFace.analyze(
@@ -9,3 +9,8 @@ obj = DeepFace.analyze(
     actions=["age","gender","emotion","race"],
     prog_bar=False,
 )
+print(obj["age"])
+emotions = obj["emotion"]
+print(max(emotions,key = emotions.get))
+print(obj['gender'])
+
