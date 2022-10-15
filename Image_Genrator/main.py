7df006e11516cd29f83a66abd0b17e05d96ab9a5
ayanalikhan4 <74181385+ayanalikhan4@users.noreply.github.com>
2022-10-15 02:14:07 -0400

    Initial

---
+++ Image_Genrator/main.py
@@ -0,0 +1,21 @@
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+import matplotlib.pyplot as plt
+
+model_id = "CompVis/stable-diffusion-v1-4"
+device = "cuda"
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    model_id, 
+    torch_dtype = torch.float32,
+    use_auth_token = True
+)
+
+prompt = ("A futuristic Starbucks barista,"
+        "full HD 4K resolution"
+)
+image = pipe(prompt,guidance_scale = 7.5)["sample"][0]
+
+plt.axis = ("off")
+plt.imshow(image);
