e6fe4e6805a36df9cceef9baf754dba409f540a1
ayanalikhan4 <74181385+ayanalikhan4@users.noreply.github.com>
2022-10-20 13:48:43 -0400

    tests

--- Image_Genrator/main.py
+++ Image_Genrator/main.py
@@ -13,12 +13,12 @@ pipe = StableDiffusionPipeline.from_pretrained(
     use_auth_token = True
 )
 
-prompt = ("An imaginary animal whith wings"
+prompt = ("An imaginary human whith wings"
         "full HD 4K resolution"
 )
 image = pipe(prompt,guidance_scale = 7.5)["sample"][0]
 
-image.save("An imaginary animal whith wings.jpg")
+image.save("An imaginary human whith wings.jpg")
 
 #plt.axis = ("off")
 #plt.imshow(image);
