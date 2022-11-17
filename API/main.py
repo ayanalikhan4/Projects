698c3529efdf60fb53c11ee97a813de2eba00a66
ayanalikhan4 <74181385+ayanalikhan4@users.noreply.github.com>
2022-11-17 11:28:27 -0500

    Update main.py

--- API/main.py
+++ API/main.py
@@ -1,6 +1,11 @@
 import requests
 
+MY_LAT = 43.452969
 
+MY_LNG = -80.495064
+
+
+"""
 response = requests.get(url = "http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
@@ -8,4 +13,18 @@ data = response.json()
 
 longitude = data["iss_position"]["longitude"]
 latitude = data["iss_position"]["latitude"]
+
+print(data)
+"""
+
+parameters = {
+    "lat":MY_LAT,
+    "lang":MY_LNG,
+
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params = parameters)
+response.raise_for_status()
+
+data = response.json()
 print(data)
